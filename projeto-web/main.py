4277549a80508133f7ca56063ccdb1cda0aa003b
phfarath <pedropontesfarath@gmail.com>
2025-03-06 21:31:40 -0300

    Adicionando o projeto-web em FLask ao projeto principal, com os modelos exportados em pkl e o design da pagina em html\css

---
+++ projeto-web/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import joblib
+
+app = Flask(__name__)
+
+# Carregar modelo e vetorizador treinados
+modelo = joblib.load(".\projeto-web\modelo.pkl")
+vectorizer = joblib.load(".\projeto-web\\vectorizer.pkl")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    sentimento = None
+    texto = ""
+
+    if request.method == "POST":
+        texto = request.form["texto"]
+        texto_vetorizado = vectorizer.transform([texto])
+        previsao = modelo.predict(texto_vetorizado)[0]
+
+        # Mapear resultado
+        sentimentos = {-1: "Negativo 😔", 0: "Neutro 😐", 1: "Positivo 😊"}
+        resultado = sentimentos[previsao]
+
+        return render_template("index.html", resultado=resultado, texto=texto)
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
